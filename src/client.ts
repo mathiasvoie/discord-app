2138f973dfc0544a6339badb484a6e9988fce8d8
Mathias Voie <mvoiej@outlook.com>
2025-08-07 22:37:06 +0200

    Update the current activity of the application to a template string.

--- src/client.ts
+++ src/client.ts
@@ -4,7 +4,7 @@ import { Client } from './classes/client';
 export const client = new Client({
   intents: [GatewayIntentBits.Guilds],
   presence: {
-    activities: [{ name: 'Listening to Lord Voie' }],
+    activities: [{ name: 'This is the activity of the application' }],
     status: 'online',
   },
 });
