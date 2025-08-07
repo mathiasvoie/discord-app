6f148152aa6c57f9eb2fd20a4faba76e5617d7c4
Mathias Voie <mvoiej@outlook.com>
2025-08-07 23:18:27 +0200

    Building the application did not work since i used await on the top level.

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import { client } from './client';
 
-await client.authenticate().then(async () => {
-  client.registerCommands().then(() => {
-    console.log('[✅] Commands registered successfully.');
+(async () => {
+  await client.authenticate().then(async () => {
+    client.registerCommands().then(() => {
+      console.log('[✅] Commands registered successfully.');
+    });
+    client.registerEvents().then(() => {
+      console.log('[✅] Events registered successfully.');
+    });
   });
-  client.registerEvents().then(() => {
-    console.log('[✅] Events registered successfully.');
-  });
-});
+})();
