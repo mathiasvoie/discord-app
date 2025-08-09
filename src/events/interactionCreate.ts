6f7622c1c1c3a367d3974b7ea5f68207709b9c5e
Mathias Voie <mvoiej@outlook.com>
2025-08-09 14:07:59 +0200

    Adding a handler for the example button added in the example embed

--- src/events/interactionCreate.ts
+++ src/events/interactionCreate.ts
@@ -1,6 +1,19 @@
 import { client } from '../client';
 
 client.on('interactionCreate', async (interaction) => {
+  // THIS IS JUST AN EXAMPLE OF HOW TO HANDLE INTERACTIONS
+  if (interaction.isButton()) {
+    const buttonId = interaction.customId;
+
+    if (buttonId === 'my_button') {
+      await interaction.reply({
+        content: 'You clicked the example button!',
+        flags: 64,
+      });
+    }
+  }
+
+  // THIS IS AN AUTOMATIC COMMAND HANDLER - DO NOT TOUCH UNLESS YOU KNOW WHAT YOU ARE DOING
   if (interaction.isCommand()) {
     const command = client.commands.get(interaction.commandName);
 
